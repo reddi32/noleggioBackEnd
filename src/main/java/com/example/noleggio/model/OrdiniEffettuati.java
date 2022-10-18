2913d0510eeb33850eb5fa52527e025745689ba3
reddi32 <rossimatteo32@gmail.com>
2022-10-18 12:04:33 +0200

    update classi

--- src/main/java/com/example/noleggio/model/OrdiniEffettuati.java
+++ src/main/java/com/example/noleggio/model/OrdiniEffettuati.java
@@ -1,13 +1,31 @@
 package com.example.noleggio.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 public class OrdiniEffettuati {
 	
 	private Integer idOrdineEffettuato;
 	private LocalDateTime dataPrenotazione;
 	private LocalDateTime dataInizio;
+	private LocalDateTime dataFine;
 	private String riepilogo;
+	
+	public OrdiniEffettuati() {}
+	
+	public OrdiniEffettuati (Integer idOrdineEffettuato, LocalDateTime dataPrenotazione, LocalDateTime dataInizio, LocalDateTime dataFine, String riepilogo) {
+		this.idOrdineEffettuato = idOrdineEffettuato;
+		this.dataPrenotazione = dataPrenotazione;
+		this.dataInizio = dataInizio;
+		this.dataFine = dataFine;
+		this.riepilogo = riepilogo;
+	}
 	
 	public Integer getIdOrdineEffettuato() {
 		return idOrdineEffettuato;
@@ -27,11 +45,43 @@ public class OrdiniEffettuati {
 	public void setDataInizio(LocalDateTime dataInizio) {
 		this.dataInizio = dataInizio;
 	}
+	public LocalDateTime getDataFine() {
+		return dataFine;
+	}
+	public void setDataFine(LocalDateTime dataFine) {
+		this.dataFine = dataFine;
+	}
 	public String getRiepilogo() {
 		return riepilogo;
 	}
 	public void setRiepilogo(String riepilogo) {
 		this.riepilogo = riepilogo;
+	}
+	
+	@ManyToOne
+    @JoinColumn(name = "fk_id_utente", referencedColumnName = "id_utente")
+    @JsonIgnoreProperties("ordineEffettuato")
+	
+	private Utenti utenti;
+	
+	public Utenti getUtenti() {
+		return utenti;
+	}
+	
+	public void setUtenti(Utenti utenti) {
+		this.utenti=utenti;
+	}
+	
+	@OneToMany( mappedBy = "ordineEffettuato" )
+    @JsonIgnoreProperties("ordineEffettuato")
+	
+	private List<UtenteRegistrato> utentiInvitati;
+		
+	public List<UtenteRegistrato> getUtentiInvitati() {
+		return utentiInvitati;
+	}
+	public void setUtentiInvitati(List<UtenteInvitato> utentiInvitati) {
+		this.utentiInvitati=utentiInvitati;
 	}
 
 }
